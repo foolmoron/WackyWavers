32e3fb263d3ca4414a0f878aaeb3dd8e048b95bc
Momin Khan <foolmoron@gmail.com>
2017-01-21 16:37:38 -0500

    Speeding mechanics

--- Assets/Scripts/WaveRider.cs
+++ Assets/Scripts/WaveRider.cs
@@ -7,9 +7,23 @@ public class WaveRider : MonoBehaviour {
     public MusicWave MusicWave;
     [Range(0, 5)]
     public float HeightSpeed = 1;
+    [Range(0, 360)]
+    public float AngleSpeed = 90;
+    public float Angle;
+
+    public GameObject RotateObj;
 
     public const int ANGLE_SAMPLES = 12;
     QueueList<float> angles = new QueueList<float>(ANGLE_SAMPLES);
+
+    public const int CORRECTNESS_SAMPLES = 1*60;
+    QueueList<float> correctness = new QueueList<float>(CORRECTNESS_SAMPLES);
+    
+    public ParticleSystem SpeedParticles;
+    public float Speed;
+    public float MinX = 1;
+    public float MaxX = 4;
+
 
     float Average(QueueList<float> list) {
         var sum = 0f;
@@ -23,16 +37,48 @@ public class WaveRider : MonoBehaviour {
         while (angles.Count < ANGLE_SAMPLES) {
             angles.Enqueue(0);
         }
+        while (correctness.Count < CORRECTNESS_SAMPLES) {
+            correctness.Enqueue(0);
+        }
     }
 
     void FixedUpdate() {
+        // do angle
+        {
+            if (Input.GetKey(KeyCode.A)) {
+                Angle -= AngleSpeed * Time.deltaTime;
+            } else if (Input.GetKey(KeyCode.D)) {
+                Angle += AngleSpeed * Time.deltaTime;
+            }
+        }
+        // angles and positions
         var currentY = MusicWave.GetHeight(transform.position.x);
-        var dX = 0.01f;
-        var dY = currentY - MusicWave.GetHeight(transform.position.x + dX);
-        angles.Dequeue();
-        angles.Enqueue(Mathf.Atan2(dY, dX) * Mathf.Rad2Deg);
-        var angle = Average(angles);
-        transform.position = Vector3.MoveTowards(transform.position, transform.position.withY(currentY), HeightSpeed * Time.deltaTime);
-        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.withZ(-angle));
+        {
+            var dX = 0.01f;
+            var dY = currentY - MusicWave.GetHeight(transform.position.x + dX);
+            angles.Dequeue();
+            angles.Enqueue(Mathf.Atan2(dY, dX) * Mathf.Rad2Deg);
+        }
+        var smoothedWaveAngle = Average(angles);
+        // correctness
+        {
+            correctness.Dequeue();
+            correctness.Enqueue(1 - (Mathf.Abs(Angle - smoothedWaveAngle) / 90));
+            var averageCorrectness = Average(correctness);
+
+            var speeding = averageCorrectness > 0.7f;
+            SpeedParticles.enableEmission(speeding);
+            Speed += Time.deltaTime * (speeding ? 1 : -1);
+            Speed = Mathf.Clamp(Speed, 0, 10);
+        }
+        // position
+        {
+            var newPos = new Vector3(Mathf.Lerp(MinX, MaxX, Speed / 10), currentY, transform.position.z);
+            transform.position = Vector3.MoveTowards(transform.position, newPos, HeightSpeed * Time.deltaTime);
+        }
+        // rotate
+        {
+            RotateObj.transform.rotation = Quaternion.Euler(RotateObj.transform.rotation.eulerAngles.withZ(-Angle));
+        }
     }
 }
