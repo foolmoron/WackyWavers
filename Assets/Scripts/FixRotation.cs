fa2082a365dc781d54d4f08a7651e0cde6fc5d4e
Momin Khan <foolmoron@gmail.com>
2017-01-21 14:46:20 -0500

    Character in game

---
+++ Assets/Scripts/FixRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixRotation : MonoBehaviour {
+
+    public float FixedZ;
+
+    void Update() {
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.withZ(FixedZ));
+    }
+}
