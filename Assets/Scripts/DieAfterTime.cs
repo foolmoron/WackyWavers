26d9797d6b3f32f66f969a102e3f69107d993549
Momin Khan <foolmoron@gmail.com>
2017-01-22 15:43:41 -0500

    Basic obstacles

---
+++ Assets/Scripts/DieAfterTime.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieAfterTime : MonoBehaviour {
+
+    public float Seconds;
+
+    void Start() {
+        Destroy(gameObject, Seconds);
+    }
+}
