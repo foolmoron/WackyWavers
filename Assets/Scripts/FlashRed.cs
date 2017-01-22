596726fce2c9901d1e863c22bb6e4133a091a019
Momin Khan <foolmoron@gmail.com>
2017-01-22 16:17:27 -0500

    Spawners

---
+++ Assets/Scripts/FlashRed.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashRed : MonoBehaviour {
+
+    [Range(0, 10f)]
+    public float PulseTime = 0.3f;
+    [Range(0, 2f)]
+    public float PulseOffset = 0f;
+
+    private SpriteRenderer spriteRenderer;
+    Color originalColor;
+    Color redColor;
+
+    void Start() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (!spriteRenderer) {
+            Destroy(this);
+            return;
+        }
+        originalColor = spriteRenderer.color;
+        redColor = new Color(spriteRenderer.color.r, spriteRenderer.color.g * 0.2f, spriteRenderer.color.b * 0.2f);
+    }
+
+    void Update() {
+        var offsetPulseTime = (Time.time + PulseOffset) % PulseTime;
+        var interp = (offsetPulseTime / PulseTime) * 2 - 1;
+        spriteRenderer.color = Color.Lerp(originalColor, redColor, interp);
+    }
+}
---
+++ Assets/Scripts/SpawnObstacles.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObstacles : MonoBehaviour {
+    
+    public GameObject[] FallingPrefabs;
+    public GameObject[] WaterPrefabs;
+
+    public float TimeBetweenSpawn = 5;
+    public float TimeToNextSpawn;
+
+    void Start() {
+
+    }
+        
+    void Update() {
+        // spawn
+        {
+            TimeToNextSpawn -= Time.deltaTime;
+            if (TimeToNextSpawn <= 0) {
+                GameObject obj = null;
+                if (Random.value > 0.5f) {
+                    // falling
+                    var thing = FallingPrefabs.Random();
+                    obj = Instantiate(thing);
+                    obj.transform.position = new Vector3(Mathf.Lerp(1, 6, Random.value), 8, -5);
+                } else {
+                    // water
+                    var thing = WaterPrefabs.Random();
+                    obj = Instantiate(thing);
+                    obj.transform.position = new Vector3(15, Mathf.Lerp(0, 2, Random.value), -5);
+                }
+                obj.AddComponent<DieAfterTime>().Seconds = 30;
+                obj.AddComponent<FlashRed>();
+                foreach (var sprite in obj.GetComponentsInChildren<SpriteRenderer>()) {
+                    sprite.gameObject.AddComponent<FlashRed>();
+                }
+                TimeToNextSpawn = TimeBetweenSpawn * (Random.value * 0.3f + 0.7f);
+            }
+        }
+    }
+}
